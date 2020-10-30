77200c17e3dad2fd88e645e78ce5a688c5c2dea4
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-30 14:02:11 +0700

    Add files via upload

---
+++ src/com/latihan/Kelipatan.java
@@ -0,0 +1,18 @@
+package com.latihan;
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Program Kelipatan Tabungan Latihan 18
+ *
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        float bilangan = 3.5f;
+        while (bilangan <= 35){
+            System.out.println(bilangan);
+            bilangan += 3.5;
+        }
+    }
+}
